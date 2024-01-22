665f0642e31adb0dbd1fb64e00d2312e0ebdea6f
naufaladdhiya <naufal.addhiya@gmail.com>
2024-01-22 21:32:08 +0700

    feat: Add JWT token generation and extraction functions & Add Authenticated middleware for authorization

--- src/helpers/Helper.ts
+++ src/helpers/Helper.ts
@@ -1,3 +1,13 @@
+import jwt from "jsonwebtoken";
+
+interface UserData {
+  name: string | null;
+  email: string | null;
+  roleId: number | null;
+  verified: boolean | null;
+  active: boolean | null;
+}
+
 const ResponseData = (
   status: number,
   message: string | null,
@@ -22,4 +32,68 @@ const ResponseData = (
   };
 };
 
-export default { ResponseData };
+const GenerateToken = (data: any): string => {
+  const token = jwt.sign(data, process.env.JWT_TOKEN as string, {
+    expiresIn: "1h",
+  });
+
+  return token;
+};
+
+const GenerateRefreshToken = (data: any): string => {
+  const token = jwt.sign(data, process.env.JWT_REFRESH_TOKEN as string, {
+    expiresIn: "7d",
+  });
+
+  return token;
+};
+
+const ExtractToken = (token: string): UserData | null => {
+  const secretKey: string = process.env.JWT_TOKEN as string;
+
+  let resData: any;
+
+  const res = jwt.verify(token, secretKey, (err, decode) => {
+    if (err) {
+      resData = null;
+    } else {
+      resData = decode;
+    }
+  });
+
+  if (resData != null) {
+    const result: UserData = <UserData>resData;
+    return result;
+  }
+
+  return null;
+};
+
+const ExtractRefreshToken = (token: string): UserData | null => {
+  const secretKey: string = process.env.JWT_REFRESH_TOKEN as string;
+
+  let resData: any;
+
+  const res = jwt.verify(token, secretKey, (err, decode) => {
+    if (err) {
+      resData = null;
+    } else {
+      resData = decode;
+    }
+  });
+
+  if (resData != null) {
+    const result: UserData = <UserData>resData;
+    return result;
+  }
+
+  return null;
+};
+
+export default {
+  ResponseData,
+  ExtractRefreshToken,
+  ExtractToken,
+  GenerateRefreshToken,
+  GenerateToken,
+};
